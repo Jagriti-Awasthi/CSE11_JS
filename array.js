f140bd5ea68a139c8455f580f8fb90f183fdd72b
Jagriti Awasthi <jagriti.25b01011285@abes.ac.in>
2026-03-10 09:37:51 +0530

    reference type

---
+++ array.js
@@ -0,0 +1,22 @@
+var myInfo=new Array();
+var myInfo1=Array();
+console.log(myInfo,myInfo);
+//array 
+let collection=[
+    {},
+    [],
+    true,
+    "john",
+    function(){},
+    9029487283,
+    undefined,
+    null,
+    new String("abc"),
+    new Date(),
+];
+console.log(collection);
+collection.teacherName="johnmac";
+collection.phoneNo=4098392091;
+console.log(collection);
+var trainer["jay",29,"lko",{isMarried:true}];
+    console.log(trainer);
---
+++ arrowFunction.js
@@ -0,0 +1,6 @@
+let add2=(a,b)=>{
+    console.log(a+b);
+};
+add2(102,20);
+let add3=(a,b)=>console.log(a+b);
+add3(30,20);
